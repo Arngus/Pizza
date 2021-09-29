e6d94f6060b739ecf741eb6a2ecf4a4cee447cf7
Arnold Gustavo <Arnold Gustavo@LAPTOP-4L0VL9RD>
2021-09-28 18:18:56 -0600

    Actualizacion Pizzeria

--- Pizzeria/Win.Pizzeria/FormMenu.cs
+++ Pizzeria/Win.Pizzeria/FormMenu.cs
@@ -16,5 +16,50 @@ namespace Win.Pizzeria
         {
             InitializeComponent();
         }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formlogin = new FormLogin();
+            formlogin.ShowDialog();
+        }
+
+        private void pedidosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nuestrasPizzasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormNuestrasPizzas = new FormNuestrasPizzas();
+            FormNuestrasPizzas.MdiParent = this;
+            FormNuestrasPizzas.Show();
+
+        }
+
+        private void nuestrosPostresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormNuestrosPostres = new FormNuestrosPostres();
+            FormNuestrosPostres.MdiParent = this;
+            FormNuestrosPostres.Show();
+        }
+
+        private void bebidasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormBebidas = new FormBebidas();
+            FormBebidas.MdiParent = this;
+            FormBebidas.Show();
+        }
+
+        private void pagoEnLineaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var FormPagos = new FormPagos();
+            FormPagos.MdiParent = this;
+            FormPagos.Show();
+        }
     }
 }
